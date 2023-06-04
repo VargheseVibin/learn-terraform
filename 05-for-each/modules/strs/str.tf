1e61d25eecd86bf42abf6ea1d08c5d1b66e3bbe9
EC2 Default User <ec2-user@ip-172-31-44-101.ap-south-1.compute.internal>
2023-06-04 17:51:11 +0000

    str changes configured

--- 05-for-each/modules/strs/str.tf
+++ 05-for-each/modules/strs/str.tf
@@ -1,10 +1,11 @@
 resource "random_string" "rand_strs" {
-    for_each = var.str_lengths_map
-    length = each.value
+  for_each = var.str_len_map
+  length   = each.value
+  special  = false
 }
 
-variable "str_lengths_map" {}
+variable "str_len_map" {}
 
 output "strings" {
-	value = random_string.rand_strs[*].result
+  value = [for str in random_string.rand_strs : str.result]
 }
