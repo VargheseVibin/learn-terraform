788af8168b33bf9800c7bc38595cfaa11f49b478
EC2 Default User <ec2-user@ip-172-31-44-101.ap-south-1.compute.internal>
2023-06-05 07:22:59 +0000

    bugfix - backed def using data block

--- 07-remote-backend/backend.tf
+++ 07-remote-backend/backend.tf
@@ -1,13 +1,5 @@
 data "aws_dynamodb_table" "dy-db-tf-state-lock-table" {
   name = "tf-state-locking-table"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
 }
 
 data "aws_s3_bucket" "s3-tf-state-s3-bucket" {
@@ -19,5 +11,5 @@ output "s3_bucket" {
 }
 
 output "dyn_db_table" {
-  value = aws_dynamodb_table.dy-db-tf-state-lock-table.name
+  value = data.aws_dynamodb_table.dy-db-tf-state-lock-table.name
 }
