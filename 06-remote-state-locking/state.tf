92c874ecf372fabffd58897c861236b7d27b690e
Vibin Varghese <vibinvvarghese@gmail.com>
2023-06-05 12:42:33 +0530

    Remote state

---
+++ 06-remote-state-locking/state.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "dy-db-tf-state-lock-table" {
+  name = "tf-state-locking-table"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+data "aws_s3_bucket" "s3-tf-state-s3-bucket" {
+  bucket = "terraform-state-storage-bucket-01"
+}
+
+output "s3_bucket" {
+  value = data.aws_s3_bucket.s3-tf-state-s3-bucket.bucket
+}
+
+output "dyn_db_table" {
+  value = aws_dynamodb_table.dy-db-tf-state-lock-table.name
+}
